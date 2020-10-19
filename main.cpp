725cdea2bad318053337d83c4dead60167befeb3
gpremo <>
2020-10-19 19:34:12 -0400

    try simpler example

--- main.cpp
+++ main.cpp
@@ -16,24 +16,30 @@ using rgb_matrix::Canvas;
 volatile bool program_interrupted = false;
 static void InterruptHandler(int signo) {
   program_interrupted = true;
-  std::cout << "Ctrl + C detected, exiting...";
+  std::cout << "Ctrl + C detected, exiting..." << std::endl;
 }
 
-void draw(Canvas *canvas, int iteration) {
+void draw(Canvas *canvas) {
 
-    float redPercentage = ((iteration % MAX_COLOR_VALUE) / MAX_COLOR_VALUE);
-    uint8_t red = (uint8_t)ceil(redPercentage * MAX_COLOR_VALUE);
+    canvas->Fill(255, 0, 0);
 
-    // draw pixels on the canvas
-    for (int y = 0; y < canvas->height(); y++) {
-        for (int x = 0; x < canvas->width(); x++) {
-            canvas->SetPixel(x, y, red, 0, 0);
-        }
+    int center_x = canvas->width() / 2;
+    int center_y = canvas->height() / 2;
+    float radius_max = canvas->width() / 2;
+    float angle_step = 1.0 / 360;
+    
+    for (float a = 0, r = 0; r < radius_max; a += angle_step, r += angle_step) {
+        if (interrupt_received)
+        return;
+        float dot_x = cos(a * 2 * M_PI) * r;
+        float dot_y = sin(a * 2 * M_PI) * r;
+        canvas->SetPixel(center_x + dot_x, center_y + dot_y, 255, 0, 0);
+        usleep(1 * 1000);  // wait a little to slow down things.
     }
 }
 
 int main(int argc, char* argv[]) {
-    std::cout << "Beginning program...\nPress Ctrl + C to exit.\n";
+    std::cout << "Beginning program...\nPress Ctrl + C to exit." << std::endl;
 
     // define defaults
     RGBMatrix::Options defaults;
@@ -42,7 +48,7 @@ int main(int argc, char* argv[]) {
     defaults.cols = 32;
     defaults.chain_length = 1;
     defaults.parallel = 1;
-    defaults.brightness = 50;
+    // defaults.brightness = 50;
 
     Canvas *canvas = RGBMatrix::CreateFromFlags(&argc, &argv, &defaults);
 
@@ -53,40 +59,41 @@ int main(int argc, char* argv[]) {
     signal(SIGTERM, InterruptHandler);
     signal(SIGINT, InterruptHandler);
 
-    int i = 0;
+    draw(canvas);
+    // int i = 0;
 
-    ssize_t frame_size = canvas->width() * canvas->height() * 3;
-    uint8_t buf[frame_size];
+    // ssize_t frame_size = canvas->width() * canvas->height() * 3;
+    // uint8_t buf[frame_size];
 
-    while (1) {
-        struct timespec start;
-        timespec_get(&start, TIME_UTC);
+    // while (1) {
+    //     struct timespec start;
+    //     timespec_get(&start, TIME_UTC);
 
-        // handle interruptions
-        ssize_t nread;
-        ssize_t total_nread = 0;
-        while ((nread = read(STDIN_FILENO, &buf[total_nread], frame_size - total_nread)) > 0) {
-            if (program_interrupted) {
-                return EXIT_FAILURE;
-            }
-            total_nread += nread;
-        }
+    //     // handle interruptions
+    //     ssize_t nread;
+    //     ssize_t total_nread = 0;
+    //     while ((nread = read(STDIN_FILENO, &buf[total_nread], frame_size - total_nread)) > 0) {
+    //         if (program_interrupted) {
+    //             return EXIT_FAILURE;
+    //         }
+    //         total_nread += nread;
+    //     }
         
-        if (total_nread < frame_size){
-            break;
-        }
+    //     if (total_nread < frame_size){
+    //         break;
+    //     }
 
-        draw(canvas, i++);
+    //     draw(canvas, i++);
 
-        // throttle the loop so we only run 60 iterations per second
-        struct timespec end;
-        timespec_get(&end, TIME_UTC);
-        long tudiff = (end.tv_nsec / 1000 + end.tv_sec * 1000000) - (start.tv_nsec / 1000 + start.tv_sec * 1000000);
+    //     // throttle the loop so we only run 60 iterations per second
+    //     struct timespec end;
+    //     timespec_get(&end, TIME_UTC);
+    //     long tudiff = (end.tv_nsec / 1000 + end.tv_sec * 1000000) - (start.tv_nsec / 1000 + start.tv_sec * 1000000);
 
-        if (tudiff < 1000000l / FPS) {
-            usleep(1000000l / FPS - tudiff);
-        }
-    }
+    //     if (tudiff < 1000000l / FPS) {
+    //         usleep(1000000l / FPS - tudiff);
+    //     }
+    // }
     
     canvas->Clear();
     delete canvas;
