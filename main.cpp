10aacfc823baf8b7b0f15c24bcb927c843aec599
gpremo <>
2020-10-21 03:09:41 -0400

    clock spin routine happens at the end of a cycle rather than the beginning

--- main.cpp
+++ main.cpp
@@ -669,7 +669,7 @@ int main(int argc, char* argv[]) {
         // Color foreground = randomColor();
         // draw(canvas, background, foreground);
         // background = foreground;
-        if (iteration++ % 100 == 0) {
+        if (iteration++ % 1000 == 999) {
             spinClock(canvas);
         }
     }
